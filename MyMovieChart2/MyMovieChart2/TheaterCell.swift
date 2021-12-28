d1a34b709f2012ade5d381e705ed55482cc7f5e6
Shin yongjun <70710995+Shin-jun@users.noreply.github.com>
2021-12-28 18:31:51 +0900

    map

---
+++ MyMovieChart2/MyMovieChart2/TheaterCell.swift
@@ -0,0 +1,16 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart2
+//
+//  Created by 신용준 on 2021/12/28.
+//
+
+import UIKit
+class TheaterCell: UITableViewCell {
+    // 상영관명
+    @IBOutlet var name: UILabel!
+    // 연락처
+    @IBOutlet var tel: UILabel!
+    // 주소
+    @IBOutlet var addr: UILabel!
+}
