623de7f19ee5ac19791c18a2a008564c189d5296
Shin yongjun <70710995+Shin-jun@users.noreply.github.com>
2021-12-30 23:30:20 +0900

    auto-layout

---
+++ AutoResizing-Practice2/AutoResizing-Practice2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoResizing-Practice2
+//
+//  Created by 신용준 on 2021/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
