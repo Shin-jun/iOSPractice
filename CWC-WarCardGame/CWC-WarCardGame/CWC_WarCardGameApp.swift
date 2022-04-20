d36ab58d7f87c2069a48056c99e8c31919d65433
EverShin <gkwk56@gmail.com>
2022-04-20 16:00:54 +0900

    CardGame 완성
    완성

---
+++ CWC-WarCardGame/CWC-WarCardGame/CWC_WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWC_WarCardGameApp.swift
+//  CWC-WarCardGame
+//
+//  Created by Shin yongjun on 2022/04/19.
+//
+
+import SwiftUI
+
+@main
+struct CWC_WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWC-WarCardGame/CWC-WarCardGame/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CWC-WarCardGame
+//
+//  Created by Shin yongjun on 2022/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
