cb579caa68a6d200103fddd44cae78763042ddb1
Shin-jun <inside1451@naver.com>
2022-06-14 02:20:59 +0900

    Add CheckAcount.swift

--- SwiftUIWebSplit/SwiftUIWebSplit/CheckAmount.swift
+++ SwiftUIWebSplit/SwiftUIWebSplit/CheckAmount.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct CheckAmount: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            }
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+        }
     }
 }
 
