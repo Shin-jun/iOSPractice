ac67a20394fcd2b5b9f30352bd9e49e92d883b23
Shin-jun <inside1451@naver.com>
2022-06-30 02:18:55 +0900

    [Add] BetterRest Start
    BetterRest 프로젝트 시작

---
+++ SwiftUIBetterRest/SwiftUIBetterRest/SwiftUIBetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetterRestApp.swift
+//  SwiftUIBetterRest
+//
+//  Created by Shin yongjun on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
